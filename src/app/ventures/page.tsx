e195880cec88109440aefa0c0cb5540ade639eae
Marcos Oliveira <olvsistemas@olvinternacional.com.br>
2025-07-25 11:15:55 -0300

    fix: remoção de import não utilizado no ventures para corrigir erro de build

--- src/app/ventures/page.tsx
+++ src/app/ventures/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from 'next/link';
 import dynamic from 'next/dynamic';
 const PlatformLayout = dynamic(() => import('../../components/platforms/PlatformLayout'));
 
